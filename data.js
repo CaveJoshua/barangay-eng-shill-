73889cde737e1ff6608fe70d3ff99f09572fbb34
unknown <nsec.fuhua.cv@gmail.com>
2026-04-07 11:14:14 +0800

    for deployment secrets for discreet protection

--- data.js
+++ data.js
@@ -119,7 +119,7 @@ router.use(helmet({
 }));
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], 
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://e52450f8.barangay-eng-shill.pages.dev'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-user-role','x-resident-id','X-XSRF-TOKEN'],
   credentials: true,
