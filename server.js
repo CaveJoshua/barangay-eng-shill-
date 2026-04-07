b7c981361b295b4ce6d44362f6e80cced2b0f7ae
unknown <nsec.fuhua.cv@gmail.com>
2026-04-07 11:40:59 +0800

    for deployment secrets for discreet protection00

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
     'http://localhost:5173', 
     'http://127.0.0.1:5173',
     'https://e52450f8.barangay-eng-shill.pages.dev',
-    'https://f155fa1b.barangay-eng-shill.pages.dev',
+    'https://0c2ee9f6.barangay-eng-shill.pages.dev',
   ], 
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-user-role', 'x-resident-id', 'X-XSRF-TOKEN'], 
